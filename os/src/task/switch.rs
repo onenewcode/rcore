14df05ce0f91a84e603546a9f99f33ff7a62df4f
onenewcode <lovestudy@qq.com>
2024-02-06 20:34:46 +0800

    rcore

---
+++ os/src/task/switch.rs
@@ -0,0 +1,11 @@
+use super::TaskContext;
+
+global_asm!(include_str!("switch.S"));
+
+
+extern "C" {
+    pub fn __switch(
+        current_task_cx_ptr: *mut TaskContext,
+        next_task_cx_ptr: *const TaskContext
+    );
+}
