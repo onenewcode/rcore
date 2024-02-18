03382d5f4fd7f737e30de97acdf9c29320bfdec4
onenewcode <lovestudy@qq.com>
2024-02-18 12:33:01 +0800

    ch5 0.2

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -1,10 +1,12 @@
+use alloc::vec::Vec;
+use lazy_static::*;
+///get app number
 pub fn get_num_app() -> usize {
     extern "C" {
         fn _num_app();
     }
     unsafe { (_num_app as usize as *const usize).read_volatile() }
 }
-
 /// get applications data
 pub fn get_app_data(app_id: usize) -> &'static [u8] {
     extern "C" {
@@ -20,4 +22,47 @@ pub fn get_app_data(app_id: usize) -> &'static [u8] {
             app_start[app_id + 1] - app_start[app_id],
         )
     }
+}
+
+lazy_static! {
+    ///All of app's name
+    static ref APP_NAMES: Vec<&'static str> = {
+        let num_app = get_num_app();
+        extern "C" {
+            fn _app_names();
+        }
+        let mut start = _app_names as usize as *const u8;
+        let mut v = Vec::new();
+        unsafe {
+            for _ in 0..num_app {
+                let mut end = start;
+                // 注意链接器会自动在每个字符串的结尾加入分隔符 \0
+                while end.read_volatile() != b'\0' {
+                    end = end.add(1);
+                }
+                let slice = core::slice::from_raw_parts(start, end as usize - start as usize);
+                let str = core::str::from_utf8(slice).unwrap();
+                v.push(str);
+                start = end.add(1);
+            }
+        }
+        v
+    };
+}
+
+#[allow(unused)]
+///get app data from name
+pub fn get_app_data_by_name(name: &str) -> Option<&'static [u8]> {
+    let num_app = get_num_app();
+    (0..num_app)
+        .find(|&i| APP_NAMES[i] == name)
+        .map(get_app_data)
+}
+///list all apps
+pub fn list_apps() {
+    println!("/**** APPS ****");
+    for app in APP_NAMES.iter() {
+        println!("{}", app);
+    }
+    println!("**************/");
 }
